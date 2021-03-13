fc765535357c64e42b2630d0ef98a5a23dec4397
Rafael Buchbinder <rafi@rbk.io>
2021-03-13 22:11:09 +0200

    proto: move *Reader reader struct to message::reader package
    To simplify naming of modules, and in preparation for adding message
    writer.

--- rsdns/src/protocol/message/mod.rs
+++ rsdns/src/protocol/message/mod.rs
@@ -2,15 +2,11 @@
 
 mod bytes;
 mod cursor;
-mod domain_name_reader;
-mod message_reader;
-mod questions_reader;
+mod reader;
 mod wcursor;
 
 pub(crate) use bytes::*;
 pub(crate) use cursor::*;
-pub(crate) use domain_name_reader::*;
-pub use message_reader::*;
-pub use questions_reader::*;
+pub use reader::*;
 #[allow(unused_imports)]
 pub(crate) use wcursor::*;
---
+++ rsdns/src/protocol/message/reader/mod.rs
@@ -0,0 +1,7 @@
+mod domain_name_reader;
+mod message_reader;
+mod questions_reader;
+
+pub(crate) use domain_name_reader::*;
+pub use message_reader::*;
+pub use questions_reader::*;
