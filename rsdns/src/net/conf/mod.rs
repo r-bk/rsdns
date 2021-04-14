cbe4969683501f3935c315b2b23525b76973d3d7
Rafael Buchbinder <rafi@rbk.io>
2021-04-14 22:37:09 +0300

    net/conf: coding: move ProtocolStrategy to its own module

--- rsdns/src/net/conf/mod.rs
+++ rsdns/src/net/conf/mod.rs
@@ -1,4 +1,6 @@
 //! Resolver configuration definitions.
 
+mod protocol_strategy;
 mod resolver_conf;
+pub use protocol_strategy::*;
 pub use resolver_conf::*;
---
+++ rsdns/src/net/conf/protocol_strategy.rs
@@ -0,0 +1,34 @@
+/// Protocol selection strategy.
+///
+/// Defines resolver's usage of UDP and TCP protocols.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub enum ProtocolStrategy {
+    /// Use UDP by default. Fallback to TCP on truncated responses.
+    ///
+    /// The default strategy is to use UDP for all queries except for
+    /// [`QType::ANY`](crate::protocol::QType::ANY).
+    /// If a UDP response is truncated, TCP is used to issue another query and receive a full
+    /// response.
+    ///
+    /// Queries that by definition are required to use only TCP are exempt from these rules.
+    Default,
+
+    /// Use UDP for all queries including [`QType::ANY`](crate::protocol::QType::ANY).
+    ///
+    /// By default [`QType::ANY`](crate::protocol::QType::ANY) queries use TCP only.
+    /// This setting will force UDP to be used for these queries too.
+    ///
+    /// Queries that by definition are required to use only TCP are exempt from these rules.
+    Udp,
+
+    /// Use TCP for all queries.
+    Tcp,
+
+    /// Do not use TCP.
+    ///
+    /// This setting forces usage of UDP only. Truncated responses are returned as is - without
+    /// being retried.
+    ///
+    /// Queries that by definition are required to use only TCP are exempt from these rules.
+    NoTcp,
+}
