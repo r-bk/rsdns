5ea63db539fed3456f96a4e16912624c9de46ca6
Rafael Buchbinder <rafi@rbk.io>
2021-04-24 16:22:15 +0300

    proto: delete the obsolete resource_record module
    Will be replaced by a more elaborate implementation.

--- rsdns/src/protocol/mod.rs
+++ rsdns/src/protocol/mod.rs
@@ -8,11 +8,9 @@ mod flags;
 mod header;
 pub mod message;
 mod question;
-mod resource_record;
 
 pub use constants::*;
 pub use domain_name::*;
 pub use flags::*;
 pub use header::*;
 pub use question::*;
-pub use resource_record::*;
--- rsdns/src/protocol/resource_record.rs
+++
@@ -1,6 +0,0 @@
-/// DNS Resource Record.
-///
-/// [RFC 1035 ~3.3](https://tools.ietf.org/html/rfc1035#section-3.3)
-pub enum ResourceRecord {
-    // todo
-}
