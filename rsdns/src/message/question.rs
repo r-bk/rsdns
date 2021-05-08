b99b70834ba9fd68fe00f04f93213e54427349d8
Rafael Buchbinder <rafi@rbk.io>
2021-05-08 23:49:44 +0300

    message/question: cleanup docs and fill missing trait implementations

--- rsdns/src/message/question.rs
+++ rsdns/src/message/question.rs
@@ -5,8 +5,12 @@ use crate::{
 };
 use std::convert::TryFrom;
 
-/// A DNS question record.
-#[derive(Debug, Clone)]
+/// Query question.
+///
+/// Questions appear in the questions section of a query to carry the parameters that define
+/// what is being asked. Usually only a single question is carried in a query, and it is
+/// copied to the response as is.
+#[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
 pub struct Question {
     /// Domain name to query.
     pub qname: DomainName,
