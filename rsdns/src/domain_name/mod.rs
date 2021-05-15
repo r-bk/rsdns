f9f7f0d2d943dbd5ae929778c8262e508d65cd58
Rafael Buchbinder <rafi@rbk.io>
2021-05-15 16:42:25 +0300

    domain_name: rename name.rs to name_arr.rs
    In preparation for renaming the DomainName class to DomainNameArr.

--- rsdns/src/domain_name/mod.rs
+++ rsdns/src/domain_name/mod.rs
@@ -1,10 +1,10 @@
 mod builder;
-mod name;
+mod name_arr;
 mod name_string;
 mod reader;
 mod writer;
 
 pub(crate) use builder::*;
-pub use name::*;
+pub use name_arr::*;
 pub use name_string::*;
 pub(crate) use reader::*;
