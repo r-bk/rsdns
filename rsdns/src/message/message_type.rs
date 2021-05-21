706ea54c1467db97bcbe76691c46f9d9cac58ff7
Rafael Buchbinder <rafi@rbk.io>
2021-05-21 22:23:18 +0300

    message_type: add conversion to a string

--- rsdns/src/message/message_type.rs
+++ rsdns/src/message/message_type.rs
@@ -1,10 +1,21 @@
+use std::fmt::{self, Display, Formatter};
+use strum_macros::IntoStaticStr;
+
 /// Message type.
-#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug, IntoStaticStr)]
 pub enum MessageType {
     /// A query message.
     Query,
     /// A response message.
     Response,
+}
+
+impl MessageType {
+    /// Converts MessageType to a static string.
+    #[inline]
+    pub fn as_str(self) -> &'static str {
+        self.into()
+    }
 }
 
 impl From<bool> for MessageType {
@@ -23,5 +34,11 @@ impl From<MessageType> for bool {
             MessageType::Query => false,
             MessageType::Response => true,
         }
+    }
+}
+
+impl Display for MessageType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_str())
     }
 }
