a0d4507e8a95a951a7e3393dea2212844368b029
Rafael Buchbinder <rafi@rbk.io>
2021-06-04 15:06:59 +0300

    domain_name: rename name_string.rs to name.rs
    In preparation for renaming the corresponding struct.

--- rsdns/src/domain_name/mod.rs
+++ rsdns/src/domain_name/mod.rs
@@ -1,13 +1,13 @@
 mod builder;
+mod name;
 mod name_arr;
 mod name_contract;
-mod name_string;
 mod reader;
 mod utils;
 mod writer;
 
 pub(crate) use builder::*;
+pub use name::*;
 pub use name_arr::*;
 pub(crate) use name_contract::*;
-pub use name_string::*;
 pub(crate) use utils::*;
