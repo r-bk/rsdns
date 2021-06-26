7886de56cf0d52784f40b448a17b2c43e4cfe905
Rafael Buchbinder <rafi@rbk.io>
2021-06-26 21:07:13 +0300

    message: disable RecordsReader-related structs
    RecordsReader is postponed to a later version.

--- rsdns/src/message/mod.rs
+++ rsdns/src/message/mod.rs
@@ -5,12 +5,16 @@ mod flags;
 mod header;
 mod message_type;
 mod operation_code;
+#[cfg(disabled)]
 mod query_class;
+#[cfg(disabled)]
 mod query_type;
 mod query_writer;
 mod question;
 pub mod reader;
+#[cfg(disabled)]
 mod record_class;
+#[cfg(disabled)]
 mod record_type;
 mod response_code;
 
@@ -18,11 +22,15 @@ pub use flags::*;
 pub use header::*;
 pub use message_type::*;
 pub use operation_code::*;
+#[cfg(disabled)]
 pub use query_class::*;
+#[cfg(disabled)]
 pub use query_type::*;
 #[allow(unused_imports)]
 pub(crate) use query_writer::*;
 pub use question::*;
+#[cfg(disabled)]
 pub use record_class::*;
+#[cfg(disabled)]
 pub use record_type::*;
 pub use response_code::*;
