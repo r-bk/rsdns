b10fd7ef9b9ee2e6b8f5654fb114cce10e9a37e9
Rafael Buchbinder <rafi@rbk.io>
2021-07-31 14:17:36 +0300

    rsdns: reorder crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,20 @@
 //! [`A`]: crate::records::data::A
 //! [`AAAA`]: crate::records::data::Aaaa
 
+//! # Library Structure
+//!
+//! *rsdns* is built from two major parts: *message parsing* and *resolvers*.
+//!
+//! The *message parsing* part is the core of *rsdns*. It is generic and is suitable for any type
+//! of resolver that you may choose. It may be used even without an *rsdns* resolver at all,
+//! if you have DNS messages obtained by other means. This part is always present and cannot
+//! be disabled.
+//!
+//! The *resolvers* part is comprised of four independent implementations
+//! of the resolver API. Usually an application will use only one of those. None of the resolvers
+//! is enabled by default. You need to enable a resolver via one of the `net-*` crate features.
+//! See the [`resolvers`] module for more information.
+
 //! # Examples
 //!
 //! The following function retrieves [`A`] records using *rsdns's* asynchronous [`tokio::Resolver`].
@@ -82,20 +96,6 @@
 //!     Ok(answer.into_rrset().rdata)
 //! }
 //! ```
-
-//! # Library Structure
-//!
-//! *rsdns* is built from two major parts: *message parsing* and *resolvers*.
-//!
-//! The *message parsing* part is the core of *rsdns*. It is generic and is suitable for any type
-//! of resolver that you may choose. It may be used even without an *rsdns* resolver at all,
-//! if you have DNS messages obtained by other means. This part is always present and cannot
-//! be disabled.
-//!
-//! The *resolvers* part is comprised of four independent implementations
-//! of the resolver API. Usually an application will use only one of those. None of the resolvers
-//! is enabled by default. You need to enable a resolver via one of the `net-*` crate features.
-//! See the [`resolvers`] module for more information.
 
 //! # `std::net::ToSocketAddrs`
 //!
