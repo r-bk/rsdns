cf78dd35ea22e3eaf1e297d4fc3073cf1b5b2319
Rafael Buchbinder <rafi@rbk.io>
2021-08-15 13:12:47 +0300

    fuzz: add RecordSet::from_msg fuzzer
    This function covers a lot of functionality: message parsing and cname
    chain resolution.

---
+++ fuzz/fuzz_targets/rrset_from_msg.rs
@@ -0,0 +1,14 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use rsdns::records::{data, RecordSet};
+
+fuzz_target!(|data: &[u8]| {
+    RecordSet::<data::A>::from_msg(data).ok();
+    RecordSet::<data::Aaaa>::from_msg(data).ok();
+    RecordSet::<data::Cname>::from_msg(data).ok();
+    RecordSet::<data::Ns>::from_msg(data).ok();
+    RecordSet::<data::Txt>::from_msg(data).ok();
+    RecordSet::<data::Ptr>::from_msg(data).ok();
+    RecordSet::<data::Mx>::from_msg(data).ok();
+    RecordSet::<data::Soa>::from_msg(data).ok();
+});
