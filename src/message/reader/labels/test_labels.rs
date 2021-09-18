d388897d75be4488ca4b689ed9e5b8ac142509e9
Rafael Buchbinder <rafi@rbk.io>
2021-09-18 08:04:43 +0300

    labels: add compressed name unit tests

--- src/message/reader/labels/test_labels.rs
+++ src/message/reader/labels/test_labels.rs
@@ -6,6 +6,46 @@ use crate::{
     Error,
 };
 
+// ; <<>> ch4 0.6.0 <<>> --read ibm.ns
+// ;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 19921
+// ;; flags: qr rd ra; QUERY: 1, ANSWER: 8, AUTHORITY: 0, ADDITIONAL: 0
+//
+// ;; QUESTION SECTION:
+// ;ibm.com.                      IN     NS
+//
+// ;; ANSWER SECTION:
+// ibm.com.                7154   IN     NS     usc2.akam.net.
+// ibm.com.                7154   IN     NS     asia3.akam.net.
+// ibm.com.                7154   IN     NS     usw2.akam.net.
+// ibm.com.                7154   IN     NS     eur2.akam.net.
+// ibm.com.                7154   IN     NS     eur5.akam.net.
+// ibm.com.                7154   IN     NS     ns1-99.akam.net.
+// ibm.com.                7154   IN     NS     ns1-206.akam.net.
+// ibm.com.                7154   IN     NS     usc3.akam.net.
+//
+// ;; Query time: 605.526µs
+// ;; SERVER: 127.0.0.53:53
+// ;; WHEN: Sat, 18 Sep 2021 07:25:20 +0300
+// ;; MSG SIZE rcvd: 191
+const IBM_NS: [u8; 191] = [
+    0x4d, 0xd1, 0x81, 0x80, 0x00, 0x01, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, // |M...........|
+    0x03, 0x69, 0x62, 0x6d, 0x03, 0x63, 0x6f, 0x6d, 0x00, 0x00, 0x02, 0x00, // |.ibm.com....|
+    0x01, 0xc0, 0x0c, 0x00, 0x02, 0x00, 0x01, 0x00, 0x00, 0x1b, 0xf2, 0x00, // |............|
+    0x0f, 0x04, 0x75, 0x73, 0x63, 0x32, 0x04, 0x61, 0x6b, 0x61, 0x6d, 0x03, // |..usc2.akam.|
+    0x6e, 0x65, 0x74, 0x00, 0xc0, 0x0c, 0x00, 0x02, 0x00, 0x01, 0x00, 0x00, // |net.........|
+    0x1b, 0xf2, 0x00, 0x08, 0x05, 0x61, 0x73, 0x69, 0x61, 0x33, 0xc0, 0x2a, // |.....asia3.*|
+    0xc0, 0x0c, 0x00, 0x02, 0x00, 0x01, 0x00, 0x00, 0x1b, 0xf2, 0x00, 0x07, // |............|
+    0x04, 0x75, 0x73, 0x77, 0x32, 0xc0, 0x2a, 0xc0, 0x0c, 0x00, 0x02, 0x00, // |.usw2.*.....|
+    0x01, 0x00, 0x00, 0x1b, 0xf2, 0x00, 0x07, 0x04, 0x65, 0x75, 0x72, 0x32, // |........eur2|
+    0xc0, 0x2a, 0xc0, 0x0c, 0x00, 0x02, 0x00, 0x01, 0x00, 0x00, 0x1b, 0xf2, // |.*..........|
+    0x00, 0x07, 0x04, 0x65, 0x75, 0x72, 0x35, 0xc0, 0x2a, 0xc0, 0x0c, 0x00, // |...eur5.*...|
+    0x02, 0x00, 0x01, 0x00, 0x00, 0x1b, 0xf2, 0x00, 0x09, 0x06, 0x6e, 0x73, // |..........ns|
+    0x31, 0x2d, 0x39, 0x39, 0xc0, 0x2a, 0xc0, 0x0c, 0x00, 0x02, 0x00, 0x01, // |1-99.*......|
+    0x00, 0x00, 0x1b, 0xf2, 0x00, 0x0a, 0x07, 0x6e, 0x73, 0x31, 0x2d, 0x32, // |.......ns1-2|
+    0x30, 0x36, 0xc0, 0x2a, 0xc0, 0x0c, 0x00, 0x02, 0x00, 0x01, 0x00, 0x00, // |06.*........|
+    0x1b, 0xf2, 0x00, 0x07, 0x04, 0x75, 0x73, 0x63, 0x33, 0xc0, 0x2a, // |.....usc3.*|
+];
+
 #[test]
 fn test_basic_flow() {
     let packet = b"\x03sub\x07example\x03com\x00";
@@ -118,4 +158,73 @@ fn test_cursor_read() {
     let dn: InlineName = cursor.read().unwrap();
 
     assert_eq!(dn.as_str(), "sub.example.com.");
+}
+
+#[test]
+fn test_labels() {
+    let expectations: &[(usize, &[&[u8]])] = &[
+        (12, &[b"ibm", b"com"]),
+        (37, &[b"usc2", b"akam", b"net"]),
+        (64, &[b"asia3", b"akam", b"net"]),
+        (84, &[b"usw2", b"akam", b"net"]),
+        (103, &[b"eur2", b"akam", b"net"]),
+        (122, &[b"eur5", b"akam", b"net"]),
+        (141, &[b"ns1-99", b"akam", b"net"]),
+        (162, &[b"ns1-206", b"akam", b"net"]),
+        (184, &[b"usc3", b"akam", b"net"]),
+    ];
+
+    for t in expectations {
+        let cursor = Cursor::new(&IBM_NS[..]);
+        let mut labels = Labels::new(cursor.clone_with_pos(t.0));
+        let ex = t.1;
+        for exl in ex {
+            let l = labels.next().unwrap().unwrap();
+            assert_eq!(&exl[..], l.bytes());
+        }
+        assert!(labels.next().is_none());
+    }
+}
+
+#[test]
+fn test_read_compressed_name() {
+    let expectations: &[(usize, &str, usize)] = &[
+        (12, "ibm.com.", 21),
+        (37, "usc2.akam.net.", 52),
+        (64, "asia3.akam.net.", 72),
+        (84, "usw2.akam.net.", 91),
+        (103, "eur2.akam.net.", 110),
+        (122, "eur5.akam.net.", 129),
+        (141, "ns1-99.akam.net.", 150),
+        (162, "ns1-206.akam.net.", 172),
+        (184, "usc3.akam.net.", 191),
+    ];
+
+    for t in expectations {
+        let mut cursor = Cursor::with_pos(&IBM_NS[..], t.0);
+        let dn: Name = read_domain_name(&mut cursor).unwrap();
+        assert_eq!(dn.as_str(), t.1);
+        assert_eq!(cursor.pos(), t.2);
+    }
+}
+
+#[test]
+fn test_skip_compressed_name() {
+    let expectations: &[(usize, usize)] = &[
+        (12, 21),
+        (37, 52),
+        (64, 72),
+        (84, 91),
+        (103, 110),
+        (122, 129),
+        (141, 150),
+        (162, 172),
+        (184, 191),
+    ];
+
+    for t in expectations {
+        let mut cursor = Cursor::with_pos(&IBM_NS[..], t.0);
+        skip_domain_name(&mut cursor).expect("skip_dn failed");
+        assert_eq!(cursor.pos(), t.1);
+    }
 }
